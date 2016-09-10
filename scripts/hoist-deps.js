0c5ffe652a31da95e4c829387ce8f94fd4737fe1
ngryman <ngryman@gmail.com>
2016-09-10 20:48:47 +0100

    :beetle: fix node 4

---
+++ scripts/hoist-deps.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+'use strict'
+const fs = require('fs')
+const exec = require('child_process').execFileSync
+const path = require('path')
+
+if (isStandalone() || canHoistDependencies()) {
+  process.exit(0)
+}
+
+hoistDeps()
+
+/* -------------------------------------------------------------------------- */
+
+function isStandalone() {
+  let ret
+
+  try {
+    const pkg = require(
+      path.resolve(__dirname, '..', '..', '..', 'package.json')
+    )
+    ret = !('meta-dev' in pkg.devDependencies)
+  }
+  catch(err) {
+    ret = true
+  }
+
+  return ret
+}
+
+function canHoistDependencies() {
+  const version = exec('npm', ['--version'], { encoding: 'utf8' })
+  return (Number(version[0]) >= 3)
+}
+
+function hoistDeps() {
+  const modulesPath = path.resolve(__dirname, '..', 'node_modules')
+  const parentModulesPath = path.resolve(__dirname, '..', '..')
+  const deps = fs.readdirSync(modulesPath)
+
+  deps.forEach(dep => {
+    if ('.bin' === dep) return
+
+    const directory = path.join(modulesPath, dep)
+    const hoistedDirectory = path.resolve(parentModulesPath, dep)
+
+    if (!fs.existsSync(hoistedDirectory)) {
+      fs.symlinkSync(directory, hoistedDirectory)
+    }
+  })
+}
